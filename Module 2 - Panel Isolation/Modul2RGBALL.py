4ff47cd60c8d5482ecba64b28cd54740793fd3ee
frederik7741 <56106659+frederik7741@users.noreply.github.com>
2025-05-26 15:34:48 +0200

    Update Modul2RGBALL.py

--- Module 2 - Panel Isolation/Modul2RGBALL.py
+++ Module 2 - Panel Isolation/Modul2RGBALL.py
@@ -20,21 +20,18 @@ def plot_combined_rgb_histogram(folder_path):
                 print(f"Warning: Failed to load {filename}")
                 continue
 
-            # Split the image into R, G, B channels
             b, g, r = cv2.split(image)
 
-            # Calculate histograms and update the combined histograms
             hist_r += cv2.calcHist([r], [0], None, [256], [0, 256]).flatten()
             hist_g += cv2.calcHist([g], [0], None, [256], [0, 256]).flatten()
             hist_b += cv2.calcHist([b], [0], None, [256], [0, 256]).flatten()
             total_pixels += r.size
 
-    # Normalize histograms
+    # Normalize
     hist_r /= total_pixels
     hist_g /= total_pixels
     hist_b /= total_pixels
 
-    # Plot combined histogram
     plt.figure(figsize=(8, 6))
     plt.plot(hist_r, color='red', label='Red')
     plt.plot(hist_g, color='green', label='Green')
